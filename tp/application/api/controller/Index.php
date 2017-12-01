3570c8890401b513538a352f776dd9f70a774541
yshen <yang.shen@anchora.com>
2017-12-01 14:52:43 +0800

    add api

---
+++ tp/application/api/controller/Index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: yshen
+ * Date: 17-12-1
+ * Time: 上午10:14
+ */
+
+class  Index {
+
+    public function  index(){
+        return "api Index index";
+    }
+}
--- tp/application/index/controller/Index.php
+++ tp/application/index/controller/Index.php
@@ -17,8 +17,47 @@ class  Index {
         config('before','beforeAction');
     }
 
-    public  function  index() {
-        return "this is index Index index";
+    public  function  index(Request $request) {
+//        $request=request();
+//        $request= Request::instance();
+//        dump($request);
+//        return "this is index Index index";
+
+        #获取request
+//        http://localhost:8888/tp/public/index/Index/index/5.html?id=a
+        dump($request->domain());
+//        http://localhost:8888
+        dump($request->pathinfo());
+//        index/Index/index/5.html
+        dump($request->path());
+//        index/Index/index/5
+
+        #请求类型
+        dump($request->method());
+        dump($request->isPost());
+        dump($request->isGet());
+        dump($request->isAjax());
+
+        #请求参数
+        dump($request->get());
+        dump($request->param());
+        dump($request->post());
+
+//        session("name","yshenhn");
+//        dump($request->session());
+//        cookie("tel","123456");
+//        dump($request->cookie());
+//        dump($request->param('type'));
+//        dump($request->cookie('tel'));
+
+        #获取模块
+        dump($request->module());
+        dump($request->controller());
+        dump($request->action());
+
+        dump($request->url());
+        dump($request->baseUrl());
+
     }
 
     public function  common(){
@@ -42,5 +81,33 @@ class  Index {
         dump($ret);
     }
 
+    public function  info($id){
+        echo url('index/Index/index',['id'=>10])."<br/>";
+        echo url('index/Index/info',['id'=>10])."<br/>";
+        return $id;
+    }
 
+    public function  httptest(Request $request){
+
+        dump($request->param());
+        $res = input('get.id');
+//            dump($request->post('id',100,'intval'));
+//            dump($request->get('id'));
+//            dump($request->get('id'),100);
+        dump($res);
+    }
+
+
+    public function  test(Request $request){
+        $res = [
+            'code' => 200,
+            'result'=>[
+                'list'=> [1,2,3,4,5]
+            ]
+        ];
+//        dump($res);
+//        return  json($res);
+        Config::set('default_return_type','json');
+        return $res;
+    }
 }
